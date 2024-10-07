b2cbec8dbecb846f8e6afa0b12b0c666b79047fe
vicolores <vcoll27@gmail.com>
2024-10-07 12:15:45 +0200

    desde JB

--- Avanzado/Ej4/Ej4.js
+++ Avanzado/Ej4/Ej4.js
@@ -8,13 +8,13 @@ triángulo rectángulo como el de más abajo, de altura el número introducido.
 *****
 */
 
-let numeroEntero = 0;
+//let numeroEntero = 0;
 
-numeroEntero = parseInt(prompt("Ingrese un numero entero: "));
+let numeroEntero = parseInt(prompt("Ingrese un numero entero: "));
 
 for (let index = 0; index <= numeroEntero; index++) {
     for (let index1 = 0; index1 < index; index1++) {
-        document.write("*");
+        document.write("H");
 
     }
     document.write("<br>")
